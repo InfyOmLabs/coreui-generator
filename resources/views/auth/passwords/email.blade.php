d45321e9a5c5f33d6ad93a90a9c39e7176f4b6ad
vishalinfyom <vishal.ribdiya@infyom.com>
2019-06-29 14:50:22 +0530

    fix: use https instead of http into scripts source

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -65,7 +65,7 @@
     </div>
 </div>
 <!-- CoreUI and necessary plugins-->
-<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.min.js"></script>
