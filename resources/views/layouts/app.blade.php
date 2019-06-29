165967a65117cdb2a1695f36a305f6e523460b6f
vishalinfyom <vishal.ribdiya@infyom.com>
2019-06-29 14:51:50 +0530

    fix: use https instead of http into scripts source

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,7 +90,7 @@
 </footer>
 </body>
 <!-- jQuery 3.1.1 -->
-<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
