233fe282d70e3e12475bb2044918e6c69cb3f5c2
ajayinfyom <ajay.makwana@infyom.com>
2020-01-20 11:10:42 +0530

    feat(auth): implement user email verification

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ Route::get('/', function () {
 });
 
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->middleware('verified');
