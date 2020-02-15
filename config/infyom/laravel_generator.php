ef7b5ddb0997271389bae5b8435e10546aec1480
ajayinfyom <ajay.makwana@infyom.com>
2020-02-15 12:22:02 +0530

    fix: Updated config file with localization

--- config/infyom/laravel_generator.php
+++ config/infyom/laravel_generator.php
@@ -50,6 +50,8 @@ return [
         'factory'           => database_path('factories/'),
 
         'modelJs'           => resource_path('assets/js/models/'),
+
+        'view_provider'     => app_path('Providers/ViewServiceProvider.php'),
     ],
 
     /*
@@ -123,6 +125,8 @@ return [
         'softDelete' => true,
 
         'save_schema_file' => true,
+
+        'localized' => false,
 
         'tables_searchable_default' => false,
 
